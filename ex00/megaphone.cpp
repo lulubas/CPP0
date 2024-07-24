adca8c837416d95a5393582652eeca6ab5dce0b0
Louis B <lb@gmail.com>
2024-07-24 19:52:36 +0200

    quick fix ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 16:18:56 by lbastien          #+#    #+#             */
-/*   Updated: 2024/03/27 17:05:39 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:52:28 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,5 @@ int main (int argc, char **argv) {
 	} else {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }
